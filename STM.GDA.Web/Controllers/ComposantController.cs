0e24cd184a110873db44f35a13f206fff8d195cf
Joey Roger <joey.roger.1@etsmtl.net>
2018-01-30 16:56:55 -0500

    Retrait des éléments du template de base. Création du modèle, de la vue et du contrôleur pour les composants.

---
+++ STM.GDA.Web/Controllers/ComposantController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace STM.GDA.Web.Controllers
+{
+    public class ComposantController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

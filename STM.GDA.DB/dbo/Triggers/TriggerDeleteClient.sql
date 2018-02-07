c5f1a2afc5cb15cf38a467447c4f694a5df981ab
Simon Lafontaine <simon.lafontaine.1@etsmtl.net>
2018-02-07 13:46:32 -0500

    Ajouter les Triggers pour les tables Client et Responsable

---
+++ STM.GDA.DB/dbo/Triggers/TriggerDeleteClient.sql
@@ -0,0 +1,8 @@
+﻿CREATE TRIGGER dbo.TriggerDeleteClient
+ON ComposantClient
+AFTER DELETE
+AS
+BEGIN
+	IF((SELECT COUNT(*) FROM ComposantClient WHERE ComposantClient.ClientId = (SELECT ClientId FROM DELETED)) = 0)
+		DELETE FROM Client WHERE ID = (SELECT ClientId FROM DELETED)
+END

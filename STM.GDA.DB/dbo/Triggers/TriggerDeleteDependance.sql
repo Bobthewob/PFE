279579c0b4fb7b7d332b974ddb35390682ee4b91
capitainBoivin <catherineboivin.cb@gmail.com>
2018-02-09 15:13:10 -0500

    Ajout du trigger pour la suppression de la dependance quand il n'y plus de composant utilisant cette dependance

---
+++ STM.GDA.DB/dbo/Triggers/TriggerDeleteDependance.sql
@@ -0,0 +1,8 @@
+﻿CREATE TRIGGER dbo.TriggerDeleteDependance
+ON ComposantDependance
+AFTER DELETE
+AS
+BEGIN
+	IF((SELECT COUNT(*) FROM ComposantDependance WHERE ComposantDependance.DependanceId = (SELECT DependanceId FROM DELETED)) = 0)
+		DELETE FROM Dependance WHERE ID = (SELECT DependanceId FROM DELETED)
+END
